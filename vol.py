b3e2a9662237ba38e5eb8621072a07609fc99425
pravinasundar <53745624+pravinasundar@users.noreply.github.com>
2019-08-09 11:36:50 +0530

    Create vol.py

---
+++ vol.py
@@ -0,0 +1,7 @@
+ca=input()
+if ca=='a' or ca=='e' or ca=='i' or ca=='o' or ca=='u':
+	print("Vowel")
+elif ca=='b' or ca=='c' or ca=='d' or ca=='f' or ca=='g' or ca=='h' or ca=='j' or ca=='k' or ca=='l' or ca=='m' or ca=='n' or ca=='p' or ca=='q' or ca=='r' or ca=='s' or ca=='t' or ca=='v' or ca=='w' or ca=='x' or ca=='y' or ca=='z':
+	print("Consonant")
+else:
+	print("invalid")
