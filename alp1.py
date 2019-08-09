5533bc5deb462d5212785d80035951f387dd9430
pravinasundar <53745624+pravinasundar@users.noreply.github.com>
2019-08-09 11:47:57 +0530

    Rename alp.py to alp1.py

---
+++ alp1.py
@@ -0,0 +1,5 @@
+cc=input()
+if cc=='a' or cc=='b' or cc=='c' or cc=='d' or cc=='e' or cc=='f' or cc=='g' or cc=='h' or cc=='i' or cc=='j' or cc=='k' or cc=='l' or cc=='m' or cc=='n' or cc=='o' or cc=='p' or cc=='q' or cc=='r' or cc=='s' or cc=='t' or cc=='u' or cc=='v' or cc=='w' or cc=='x' or cc=='y' or cc=='z':
+	print("Alphabet")
+else:
+	print("No")
