872b1ba305c3d4581021cf0b699fe483cc9b5f78
pravinasundar <53745624+pravinasundar@users.noreply.github.com>
2019-08-14 13:40:08 +0530

    Update amr1.py

--- amr1.py
+++ amr1.py
@@ -1,11 +1,11 @@
-n=int(input())
+n1=int(input())
 sum =0
-temp = n
+temp = n1
 while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp //= 10
-if n == sum:
+if n1 == sum:
    print("yes")
 else:
    print("no")
